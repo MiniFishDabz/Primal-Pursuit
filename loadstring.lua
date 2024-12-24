b27e45bca8d0f010942c68aaacf9513b9ea09261
MiniFishDabz <149769887+MiniFishDabz@users.noreply.github.com>
2024-12-24 11:53:54 +0100

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/MiniFishDabz/Primal-Pursuit/refs/heads/main/Script.lua", true)()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MiniFishDabz/Primal-Pursuit/refs/heads/main/Script.lua", true))()
